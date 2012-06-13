90366059f617b6c67fb08d1e4ad7d99bafdf0c42
Fire30 <tjcorley30@hotmail.com>
2012-06-12 21:32:56 -0400

    added sleep, thanks to pcfreak

--- dll_load/dll_load.cpp
+++ dll_load/dll_load.cpp
@@ -1,22 +1,31 @@
 #include "stdafx.h"
 #include <stdio.h>
 
-typedef void (*SV_GameSendServerCommand_t)(int clientNum, /*svscmd_type*/int type, const char *text);
+
+typedef void (*SV_GameSendServerCommand_t)(int clientNum, /*svscmd_type*/char type, const char *text);
 typedef int (*Key_IsDown)(int ClientNum, int ButtonEnum);
+
 
 DWORD WINAPI commands(LPVOID)
 {
 		SV_GameSendServerCommand_t SV_GameSendServerCommand = (SV_GameSendServerCommand_t)0x822548C8;
 		Key_IsDown button_Pressed = (Key_IsDown)0x82141308;
+
         for(;;)
-                {		
-						if(button_Pressed(0,0x06)) // right shoulder
+                {
+					if(button_Pressed(0,0x06)) // Right Shoulder
                         {
-                                SV_GameSendServerCommand(0,0, "s r_fullbright \"1\"");
-								//printf("Right Shoulder Pressed");
+							    SV_GameSendServerCommand(0,0, "s r_fullbright \"1\"");
+								SV_GameSendServerCommand(0,0, "c \"RIGHT SHOULDER PRESSED\"");
+								printf("Right Shoulder Pressed \n");
+								Sleep(1000);
 						}
 						
 }
+}
+
+void multiplybytwo()
+{
 }
 
 BOOL WINAPI DllMain(HANDLE hInstDLL, DWORD reason, LPVOID lpReserved)

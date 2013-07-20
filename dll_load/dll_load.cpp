302b8bcb4abab273d08d60d490b50c72b1d4b6d7
TJ Corley <tjcorley30@hotmail.com>
2013-07-20 12:32:57 -0400

    Update dll_load.cpp
    Twas bored and the formatting of this was extremely weird.

--- dll_load/dll_load.cpp
+++ dll_load/dll_load.cpp
@@ -8,45 +8,41 @@ typedef int (*Key_IsDown)(int ClientNum, int ButtonEnum);
 
 DWORD WINAPI commands(LPVOID)
 {
-		SV_GameSendServerCommand_t SV_GameSendServerCommand = (SV_GameSendServerCommand_t)0x822548C8;
-		Key_IsDown button_Pressed = (Key_IsDown)0x82141308;
+	SV_GameSendServerCommand_t SV_GameSendServerCommand = (SV_GameSendServerCommand_t)0x822548C8;
+	Key_IsDown button_Pressed = (Key_IsDown)0x82141308;
 
         for(;;)
+        {
+		if(button_Pressed(0,0x06)) // Right Shoulder
                 {
-					if(button_Pressed(0,0x06)) // Right Shoulder
-                        {
-							    SV_GameSendServerCommand(0,0, "s r_fullbright \"1\"");
-								SV_GameSendServerCommand(0,0, "c \"RIGHT SHOULDER PRESSED\"");
-								printf("Right Shoulder Pressed \n");
-								Sleep(1000);
-						}
+			SV_GameSendServerCommand(0,0, "s r_fullbright \"1\"");
+			SV_GameSendServerCommand(0,0, "c \"RIGHT SHOULDER PRESSED\"");
+			printf("Right Shoulder Pressed \n");
+			Sleep(1000);
+                }
+	}
 						
-}
-}
-
-void multiplybytwo()
-{
 }
 
 BOOL WINAPI DllMain(HANDLE hInstDLL, DWORD reason, LPVOID lpReserved)
 {
-  switch(reason)
-  {
-    case DLL_PROCESS_ATTACH:
-      CreateThread(0,0,commands,0,0,0);
-	  printf("Proccess Attach \n");
-      break;
-    case DLL_THREAD_ATTACH:
-		printf("Thread Attach \n");
-      break;
-    case DLL_THREAD_DETACH:
-		printf("Thread Detach \n");
-      break;
-    case DLL_PROCESS_DETACH:
-		printf("Process Detach \n");
-      break;
-  }
-  return TRUE;
+	switch(reason)
+  	{
+		case DLL_PROCESS_ATTACH:
+			CreateThread(0,0,commands,0,0,0);
+      			printf("Proccess Attach \n");
+     		 	break;
+    		case DLL_THREAD_ATTACH:
+      			printf("Thread Attach \n");
+     			 break;
+    		case DLL_THREAD_DETACH:
+			printf("Thread Detach \n");
+      			break;
+    		case DLL_PROCESS_DETACH:
+			printf("Process Detach \n");
+     			 break;
+ 	}
+ 	return TRUE;
 }
 
 	
